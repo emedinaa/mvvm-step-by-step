e50b0eace6e08784471460ec7f6e049f2b19a487
Eduardo José Medina Alfaro <emedinaa@gmail.com>
2020-05-13 16:11:33 -0500

    Added viewbinding sample

--- KotlinMVVM/app/src/main/java/com/emedinaa/kotlinmvvm/di/Injection.kt
+++ KotlinMVVM/app/src/main/java/com/emedinaa/kotlinmvvm/di/Injection.kt
@@ -1,21 +1,9 @@
 package com.emedinaa.kotlinmvvm.di
 
-import androidx.lifecycle.ViewModelProvider
 import com.emedinaa.kotlinmvvm.model.MuseumDataSource
 import com.emedinaa.kotlinmvvm.model.MuseumRepository
-import com.emedinaa.kotlinmvvm.viewmodel.MuseumViewModel
-import com.emedinaa.kotlinmvvm.viewmodel.ViewModelFactory
 
 object Injection {
-
-    private val museumDataSource:MuseumDataSource = MuseumRepository()
-    private val museumViewModelFactory = ViewModelFactory(museumDataSource)
-
-    fun providerRepository():MuseumDataSource{
-        return museumDataSource
-    }
-
-    fun provideViewModelFactory(): ViewModelProvider.Factory{
-        return museumViewModelFactory
-    }
+    private val museumRepository = MuseumRepository()
+    fun providerRepository():MuseumDataSource= museumRepository
 }
--- KotlinMVVM/app/src/main/java/com/emedinaa/kotlinmvvm/view/MuseumActivity.kt
+++ KotlinMVVM/app/src/main/java/com/emedinaa/kotlinmvvm/view/MuseumActivity.kt
@@ -7,32 +7,26 @@ import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.emedinaa.kotlinmvvm.R
+import com.emedinaa.kotlinmvvm.databinding.ActivityMuseumBinding
 import com.emedinaa.kotlinmvvm.di.Injection
 import com.emedinaa.kotlinmvvm.model.Museum
 import com.emedinaa.kotlinmvvm.viewmodel.MuseumViewModel
-import kotlinx.android.synthetic.main.activity_museum.*
-import kotlinx.android.synthetic.main.layout_error.*
+import com.emedinaa.kotlinmvvm.viewmodel.ViewModelFactory
 
 class MuseumActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MuseumViewModel
     private lateinit var adapter: MuseumAdapter
+    private lateinit var binding:ActivityMuseumBinding
 
     companion object {
         const val TAG= "CONSOLE"
     }
 
-    /**
-     //Consider this, if you need to call the service once when activity was created.
-        Log.v(TAG,"savedInstanceState $savedInstanceState")
-        if(savedInstanceState==null){
-            viewModel.loadMuseums()
-        }
-     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_museum)
+        binding = ActivityMuseumBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setupViewModel()
         setupUI()
@@ -41,29 +35,13 @@ class MuseumActivity : AppCompatActivity() {
     //ui
     private fun setupUI(){
         adapter= MuseumAdapter(viewModel.museums.value?: emptyList())
-        recyclerView.layoutManager= LinearLayoutManager(this)
-        recyclerView.adapter= adapter
+        binding.recyclerView.layoutManager= LinearLayoutManager(this)
+        binding.recyclerView.adapter= adapter
     }
 
     //viewmodel
-    /**
-        //Consider this if ViewModel class don't require parameters.
-        viewModel = ViewModelProviders.of(this).get(MuseumViewModel::class.java)
-
-        //if you require any parameters to  the ViewModel consider use a ViewModel Factory
-        //viewModel = ViewModelProviders.of(this,ViewModelFactory(Injection.providerRepository())).get(MuseumViewModel::class.java)
-        viewModel = ViewModelProvider(this,Injection.provideViewModelFactory()).get(MuseumViewModel::class.java)
-
-        //Anonymous observer implementation
-        viewModel.museums.observe(this,Observer<List<Museum>> {
-            Log.v("CONSOLE", "data updated $it")
-            adapter.update(it)
-        })
-     */
     private fun setupViewModel(){
-        viewModel = ViewModelProvider(this,Injection.provideViewModelFactory()).get(MuseumViewModel::class.java)
-        //viewModel = ViewModelProvider(this,ViewModelFactory(Injection.providerRepository())).get(MuseumViewModel::class.java)
-        //viewModel = ViewModelProviders.of(this,ViewModelFactory(Injection.providerRepository())).get(MuseumViewModel::class.java)
+        viewModel = ViewModelProvider(this,ViewModelFactory(Injection.providerRepository())).get(MuseumViewModel::class.java)
         viewModel.museums.observe(this,renderMuseums)
 
         viewModel.isViewLoading.observe(this,isViewLoadingObserver)
@@ -74,28 +52,28 @@ class MuseumActivity : AppCompatActivity() {
     //observers
     private val renderMuseums= Observer<List<Museum>> {
         Log.v(TAG, "data updated $it")
-        layoutError.visibility=View.GONE
-        layoutEmpty.visibility=View.GONE
+        binding.layoutError.root.visibility=View.GONE
+        binding.layoutEmpty.root.visibility=View.GONE
         adapter.update(it)
     }
 
     private val isViewLoadingObserver= Observer<Boolean> {
         Log.v(TAG, "isViewLoading $it")
         val visibility=if(it)View.VISIBLE else View.GONE
-        progressBar.visibility= visibility
+        binding.progressBar.visibility= visibility
     }
 
     private val onMessageErrorObserver= Observer<Any> {
         Log.v(TAG, "onMessageError $it")
-        layoutError.visibility=View.VISIBLE
-        layoutEmpty.visibility=View.GONE
-        textViewError.text= "Error $it"
+        binding.layoutError.root.visibility=View.VISIBLE
+        binding.layoutEmpty.root.visibility=View.GONE
+        binding.layoutError.textViewError.text= "Error $it"
     }
 
     private val emptyListObserver= Observer<Boolean> {
         Log.v(TAG, "emptyListObserver $it")
-        layoutEmpty.visibility=View.VISIBLE
-        layoutError.visibility=View.GONE
+        binding.layoutEmpty.root.visibility=View.VISIBLE
+        binding.layoutError.root.visibility=View.GONE
     }
 
      //If you require updated data, you can call the method "loadMuseum" here
